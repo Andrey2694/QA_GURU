bb63c38e0ffee3e7eb689495b666b35feac548c8
Andrey2694 <fordcoppola94@gmail.com>
2021-10-31 11:11:50 +0100

    add check popup

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -3,11 +3,14 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SearchTests {
+public class SearchTest {
     @Test
     public void selenideSearchTest() {
         open("https://www.google.com");
-        $x(("//div[@class = 'jyfHyd'][text() = 'Принимаю']")).click();
+
+        if ($("#CXQnmb").isDisplayed()) {
+            $x(("//div[@class = 'jyfHyd'][text() = 'Принимаю']")).click();
+        }
         $("[name=q]").setValue("Selenide").pressEnter();
         $("[id = search]").shouldHave(text("selenide.org"));
     }
