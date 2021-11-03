5c70d5205ff421032e9ffa1a0461df345a962b75
Andrey2694 <fordcoppola94@gmail.com>
2021-11-03 20:18:28 +0100

    new test DragAndDropTest

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @Test
+    public void dragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+    }
+
+}
