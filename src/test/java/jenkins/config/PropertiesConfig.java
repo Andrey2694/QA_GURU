7fc6b5d99085184b0e43d724892b804bd41ded0b
Alexey <vassilyevalexey@gmail.com>
2021-11-29 13:59:40 +0600

    classpath:config/credentials.properties

--- src/test/java/jenkins/config/PropertiesConfig.java
+++ src/test/java/jenkins/config/PropertiesConfig.java
@@ -2,7 +2,7 @@ package jenkins.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:config/config.properties"})
+@Config.Sources({"classpath:config/credentials.properties"})
 public interface PropertiesConfig extends Config {
     //String url();
     String login();
